24b9ee59c211b3961244a0cdeae36d21725d9764
Joe Kramer <joe.kramer.cu@gmail.com>
2015-09-20 02:53:04 -0600

    Add another test

--- spec/clang-ffi-spec.js
+++ spec/clang-ffi-spec.js
@@ -26,9 +26,21 @@ describe('libclang', () => {
       const tunit = libclang.clang_createTranslationUnitFromSourceFile(
           index, 'input/deadbeef.cpp', 0, 0, 0, 0);
       expect(tunit).toBeDefined();
-      expect(index).not.toEqual(0);
+      expect(tunit).not.toEqual(0);
+
       const numDiags = libclang.clang_getNumDiagnostics(tunit);
       expect(numDiags).toBeGreaterThan(0);
+
+      const diag = libclang.clang_getDiagnostic(tunit, 0);
+      expect(diag).toBeDefined();
+      expect(diag).not.toEqual(0);
+      libclang.clang_disposeDiagnostic(diag);
+
+      const diagMessage = libclang.clang_formatDiagnostic(
+          diag, libclang.clang_defaultDiagnosticDisplayOptions());
+      expect(diagMessage.toString().length).toBeGreaterThan(0);
+      libclang.clang_disposeString(diagCxString);
+
       libclang.clang_disposeTranslationUnit(tunit);
     });
   });
