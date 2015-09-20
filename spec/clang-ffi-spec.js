a380b555627a8b830710d7a4429040d3fdcbb82b
Joe Kramer <joe.kramer.cu@gmail.com>
2015-09-20 02:43:39 -0600

    Add a few tests

--- spec/clang-ffi-spec.js
+++ spec/clang-ffi-spec.js
@@ -1,6 +1,6 @@
 'use babel';
 
-import libclang from '../lib/clang-ffi'
+import libclang from '../lib/clang-ffi';
 
 describe('libclang', () => {
   it('should create an index', () => {
@@ -8,5 +8,28 @@ describe('libclang', () => {
     expect(index).toBeDefined();
     expect(index).not.toEqual(0);
     libclang.clang_disposeIndex(index);
+  });
+  describe('translation unit operations', () => {
+    let index;
+    beforeEach(() => { index = libclang.clang_createIndex(0, 0); });
+    afterEach(() => { libclang.clang_disposeIndex(index); });
+
+    it('should compile a valid c++ file', () => {
+      const tunit = libclang.clang_createTranslationUnitFromSourceFile(
+          index, 'input/hello.cpp', 0, 0, 0, 0);
+      expect(tunit).toBeDefined();
+      expect(index).not.toEqual(0);
+      libclang.clang_disposeTranslationUnit(tunit);
+    });
+
+    it('should diagnose an invalid c++ file', () => {
+      const tunit = libclang.clang_createTranslationUnitFromSourceFile(
+          index, 'input/deadbeef.cpp', 0, 0, 0, 0);
+      expect(tunit).toBeDefined();
+      expect(index).not.toEqual(0);
+      const numDiags = libclang.clang_getNumDiagnostics(tunit);
+      expect(numDiags).toBeGreaterThan(0);
+      libclang.clang_disposeTranslationUnit(tunit);
+    });
   });
 });
